8d67bb71b014a5ab392a11e9662bc7bf44de7f36
SuperTerminalux <superterminalux@gmail.com>
2026-05-09 18:29:58 +0300

    Some improvements have been made

--- src/CodingBat/Warmup_1/Q03SumDouble.java
+++ src/CodingBat/Warmup_1/Q03SumDouble.java
@@ -7,7 +7,6 @@ public class Q03SumDouble {
     //Defining the method which is takes inputs and returns the sum of the two numbers or double the sum if they are equal.
     public static int sumDouble(int a, int b) {
 
-
         int result = a+b;
 
         if (a==b) {
@@ -21,6 +20,6 @@ public class Q03SumDouble {
     public static void main(String[] args) {
         System.out.println("sumDouble(1, 2) -> " + sumDouble(1, 2));//Case 1
         System.out.println("sumDouble(3, 2) -> " + sumDouble(3, 2));//Case 2
-        System.out.println("sumDouble(3, 2) -> " + sumDouble(2, 2));//Case 3
+        System.out.println("sumDouble(2, 2) -> " + sumDouble(2, 2));//Case 3
     }
 }
